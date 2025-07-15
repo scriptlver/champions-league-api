390565d1980e53e1d9a4f72f777b1d350326ffcb
scriptlver <sloowlaah1@gmail.com>
2025-07-15 17:56:15 -0300

    sii

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,6 +1,14 @@
-
-
+import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async ()=>{
-    return { player: "messi" };
+    const data = { player: "messi"};
+    let response = null;
+
+    if(data){
+        response = await ok(data);
+    }else{
+        response = await noContent();
+
+    }
+    return response;
 }
