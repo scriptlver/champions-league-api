e92b8de4f62a391583408fd13789a1b1297769e7
scriptlver <sloowlaah1@gmail.com>
2025-07-15 11:20:00 -0300

    aaaaaaaaa

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import express, {Request, Response} from 'express';
+import express, {Request, Response, json} from 'express';
 
 
 const app = express();
+const port = process.env.PORT;
+
+app.use(json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Champions League API!');
+    res.status(200).json({player: "beckham"});
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
